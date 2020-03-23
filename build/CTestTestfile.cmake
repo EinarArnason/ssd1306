5e0d551784aa85418dcbc318f3c7ad21d6a04157
Einar Arnason <einsiarna@gmail.com>
2020-03-23 20:33:23 +0000

    Remove build files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/einar/Mikro/waterboy2/waterboy2-interface/ssd1306
-# Build directory: /home/einar/Mikro/waterboy2/waterboy2-interface/ssd1306/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
