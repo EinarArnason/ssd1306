32fa39801848118910ae161297f68f2024e5b29e
Einar Arnason <einsiarna@gmail.com>
2020-04-24 01:43:09 +0000

    Refactoring

--- include/SSD1306.h
+++ include/SSD1306.h
@@ -7,121 +7,130 @@
 #include <string.h>
 
 namespace SSD1306 {
-    static const unsigned char I2C_ADDRESS = 0x3c;
+static const unsigned char I2C_ADDRESS = 0x3c;
 
-    // Set the order of memory addressing
-    static const unsigned char SET_MEMORY_ADDRESSING_MODE = 0x20;
-    // Every column is addressed for each page
-    static const unsigned char MEMORY_ADDRESSING_HORIZONTAL = 0x00;
-    // Every page is addressed for each column
-    static const unsigned char MEMORY_ADDRESSING_VERTICAL = 0x01;
-    // Every column is addressed for a single page
-    static const unsigned char MEMORY_ADDRESSING_PAGE = 0x10;
+// Set the order of memory addressing
+static const unsigned char SET_MEMORY_ADDRESSING_MODE = 0x20;
+// Every column is addressed for each page
+static const unsigned char MEMORY_ADDRESSING_HORIZONTAL = 0x00;
+// Every page is addressed for each column
+static const unsigned char MEMORY_ADDRESSING_VERTICAL = 0x01;
+// Every column is addressed for a single page
+static const unsigned char MEMORY_ADDRESSING_PAGE = 0x10;
 
-    static const unsigned char SET_CONTRAST = 0x81;
-    static const unsigned char DEFAULT_CONTRAST = 0x7f;
+static const unsigned char SET_CONTRAST = 0x81;
+static const unsigned char DEFAULT_CONTRAST = 0x7f;
 
-    // Specifies column start address and end address of the display data RAM.
-    static const unsigned char SET_COLUMN_ADDRESS = 0x21;
-    // Specifies page start address and end address of the display data RAM
-    static const unsigned char SET_PAGE_ADDRESS = 0x22;
-    
-    // Charge pump regulator setting
-    static const unsigned char CHARGE_PUMP_SETTING = 0x8D;
-    static const unsigned char CHARGE_PUMP_ENABLE = 0x14;
-    static const unsigned char CHARGE_PUMP_DISABLE = 0x10;
+// Specifies column start address and end address of the display data RAM.
+static const unsigned char SET_COLUMN_ADDRESS = 0x21;
+// Specifies page start address and end address of the display data RAM
+static const unsigned char SET_PAGE_ADDRESS = 0x22;
 
-    static const unsigned char SET_SEGMENT_REMAP_OFF = 0xA0;
-    static const unsigned char SET_SEGMENT_REMAP_ON = 0xA1;
+// Charge pump regulator setting
+static const unsigned char CHARGE_PUMP_SETTING = 0x8D;
+static const unsigned char CHARGE_PUMP_ENABLE = 0x14;
+static const unsigned char CHARGE_PUMP_DISABLE = 0x10;
 
-    static const unsigned char DISPLAYALLON_RESUME = 0xA4;
-    static const unsigned char DISPLAYALLON = 0xA5;
+static const unsigned char SET_SEGMENT_REMAP_OFF = 0xA0;
+static const unsigned char SET_SEGMENT_REMAP_ON = 0xA1;
 
-    static const unsigned char NORMAL_DISPLAY = 0xA6;
-    static const unsigned char INVERT_DISPLAY = 0xA7;
+static const unsigned char ENTIRE_DISPLAY_ON_RESUME_RAM = 0xA4;
+static const unsigned char ENTIRE_DISPLAY_ON_IGNORE_RAM = 0xA5;
 
-    static const unsigned char SET_MUX_RATIO = 0xA8;
+static const unsigned char NORMAL_DISPLAY = 0xA6;
+static const unsigned char INVERT_DISPLAY = 0xA7;
 
-    static const unsigned char DISPLAY_OFF = 0xAE;
-    static const unsigned char DISPLAY_ON = 0xAF;
+static const unsigned char SET_MUX_RATIO = 0xA8;
 
-    static const unsigned char SET_COM_OUTPUT_SCAN_NORMAL = 0xC0;
-    static const unsigned char SET_COM_OUTPUT_SCAN_REMAPPED = 0xC8;
+static const unsigned char DISPLAY_OFF = 0xAE;
+static const unsigned char DISPLAY_ON = 0xAF;
 
-    static const unsigned char SET_DISPLAY_OFFSET = 0xD3;
+static const unsigned char SET_COM_OUTPUT_SCAN_NORMAL = 0xC0;
+static const unsigned char SET_COM_OUTPUT_SCAN_REMAPPED = 0xC8;
 
-    static const unsigned char SET_DISPLAY_CLOCK_DIVIDER = 0xD5;
-    static const unsigned char DEFAULT_DISPLAY_CLOCK_DIVIDER = 0x80;
+static const unsigned char SET_DISPLAY_OFFSET = 0xD3;
 
-    static const unsigned char SET_PRECHARGE_PERIOD = 0xD9;
-    static const unsigned char SET_COM_PINS_HW_CONFIG = 0xDA;
-    static const unsigned char SET_VCOM_DESELECT_LEVEL = 0xDB;
+// Set Display Clock Divide Ratio/Oscillator Frequency
+static const unsigned char SET_DISPLAY_CLOCK_DIVIDER = 0xD5;
+static const unsigned char DEFAULT_DISPLAY_CLOCK_DIVIDER = 0x80;
 
-    static const unsigned char SETLOWCOLUMN = 0x00;
-    static const unsigned char SETHIGHCOLUMN = 0x10;
-    static const unsigned char SET_START_LINE = 0x40;
+static const unsigned char SET_PRECHARGE_PERIOD = 0xD9;
 
-    static const unsigned char EXTERNALVCC = 0x01;
-    static const unsigned char SWITCHCAPVCC = 0x02;
+static const unsigned char SET_COM_PINS_HW_CONFIG = 0xDA;
+static const unsigned char COM_PINS_SEQUENTIAL = 0x02;
+static const unsigned char COM_PINS_SEQUENTIAL_LEFTRIGHT_REMAP = 0x22;
+static const unsigned char COM_PINS_ALTERNATIVE = 0x12;
+static const unsigned char COM_PINS_ALTERNATIVE_LEFTRIGHT_REMAP = 0x32;
 
-    static const unsigned char RIGHT_HORIZONTAL_SCROLL = 0x26;
-    static const unsigned char LEFT_HORIZONTAL_SCROLL = 0x27;
-    static const unsigned char VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
-    static const unsigned char VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
-    static const unsigned char DEACTIVATE_SCROLL = 0x2E;
-    static const unsigned char ACTIVATE_SCROLL = 0x2F;
-    static const unsigned char SET_VERTICAL_SCROLL_AREA = 0xA3;
+static const unsigned char SET_VCOM_DESELECT_LEVEL = 0xDB;
 
-    static const unsigned char COMMAND = 0x00;
-    static const unsigned char DATA = 0x40;
+static const unsigned char SETLOWCOLUMN = 0x00;
+static const unsigned char SETHIGHCOLUMN = 0x10;
+static const unsigned char SET_START_LINE = 0x40;
 
-    static const char DEGREE_SYMBOL = 127;
-    static const int ASCII_OFFSET = 32;
+static const unsigned char EXTERNALVCC = 0x01;
+static const unsigned char SWITCHCAPVCC = 0x02;
 
-    class LCD {
-      private:
-        I2C* i2c;
-        int width;
-        int height;
-        int size;
-        bool isOn;
-        bool inverted;
-        unsigned char contrast;
-        I2C::Config i2cConfig;
+static const unsigned char RIGHT_HORIZONTAL_SCROLL = 0x26;
+static const unsigned char LEFT_HORIZONTAL_SCROLL = 0x27;
+static const unsigned char VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
+static const unsigned char VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
+static const unsigned char DEACTIVATE_SCROLL = 0x2E;
+static const unsigned char ACTIVATE_SCROLL = 0x2F;
+static const unsigned char SET_VERTICAL_SCROLL_AREA = 0xA3;
 
-      public:
-        LCD(I2C* i2c = nullptr, int width = 128, int height = 64,
-            unsigned long frequency = 400000U,
-            unsigned char address = I2C_ADDRESS);
-        ~LCD();
-        bool init();
-        bool sendData(const unsigned char* data, int size);
-        bool sendCommand(const unsigned char cmd);
-        /*
-            Image must be flipped and top must be oriented left because byte
-           orientation in SSD1306 RAM is vertical, width exceeding LCD height is
-           fine.
-        */
-        bool drawXbitmap(const unsigned char* bitmap, int x, int y, int width,
-                         int height);
-        bool drawXbitmap(Graphic* graphic);
-        bool drawXbitmap(Graphic graphic);
-        bool writeText(const char* text, int length);
-        bool setWritingArea(int x, int y, int width, int height);
-        bool setWritingArea(TextBox textBox);
-        bool fillRectangle(int x, int y, int width, int height, bool color);
-        bool print(TextBox textBox, const char* text);
-        bool clearScreen();
-        bool turnOff();
-        bool turnOn();
-        bool lcdOn();
-        bool setContrast(unsigned char value);
-        bool setMultiplexRatio(unsigned char ratio);
-        bool setMemoryAddressingMode(unsigned char mode);
-        bool setColumnRange(unsigned char start, unsigned char end);
-        bool setPageRange(unsigned char start, unsigned char end);
-        bool invert();
-        int lcdWidth();
-        int lcdHeight();
-    };
-} // namespace SSD1306
+static const unsigned char COMMAND = 0x00;
+static const unsigned char DATA = 0x40;
+
+static const char DEGREE_SYMBOL = 127;
+static const int ASCII_OFFSET = 32;
+
+class LCD {
+ private:
+  I2C* i2c;
+  int width;
+  int height;
+  int size;
+  bool isOn;
+  bool inverted;
+  unsigned char contrast;
+  I2C::Config i2cConfig;
+
+ public:
+  LCD(I2C* i2c = nullptr, int width = 128, int height = 64,
+      unsigned long frequency = 400000U, unsigned char address = I2C_ADDRESS);
+  ~LCD();
+  bool init();
+  bool sendData(const unsigned char* data, int size);
+  bool sendCommand(const unsigned char cmd);
+  /*
+      Image must be flipped and top must be oriented left because byte
+     orientation in SSD1306 RAM is vertical, width exceeding LCD height is
+     fine.
+  */
+  bool drawXbitmap(const unsigned char* bitmap, int x, int y, int width,
+                   int height);
+  bool drawXbitmap(Graphic* graphic);
+  bool drawXbitmap(Graphic graphic);
+  bool writeText(const char* text, int length);
+  bool setWritingArea(int x, int y, int width, int height);
+  bool setWritingArea(TextBox textBox);
+  bool fillRectangle(int x, int y, int width, int height, bool color);
+  bool print(TextBox textBox, const char* text);
+  bool clearScreen();
+  bool turnOff();
+  bool turnOn();
+  bool lcdIsOn();
+  bool setContrast(unsigned char value);
+  bool entireDisplayOn(bool resumeRam = true);
+  bool setMultiplexRatio(unsigned char ratio);
+  bool setMemoryAddressingMode(unsigned char mode);
+  bool setColumnRange(unsigned char start, unsigned char end);
+  bool setPageRange(unsigned char start, unsigned char end);
+  bool setChargePumpSetting(unsigned char setting);
+  // Invert color of the entire screen
+  bool invert();
+  int lcdWidth();
+  int lcdHeight();
+};
+}  // namespace SSD1306
--- src/SSD1306.cpp
+++ src/SSD1306.cpp
@@ -2,298 +2,321 @@
 
 SSD1306::LCD::LCD(I2C* i2c, int width, int height, unsigned long frequency,
                   unsigned char address) {
-    this->i2c = i2c;
-    this->width = width;
-    this->height = height;
-    // Divided with bits per byte
-    this->size = width * height / 8;
-    this->inverted = false;
-    this->i2cConfig.address = address;
-    this->i2cConfig.frequency = frequency;
+  this->i2c = i2c;
+  this->width = width;
+  this->height = height;
+  // Divided with bits per byte
+  this->size = width * height / 8;
+  this->inverted = false;
+  this->i2cConfig.address = address;
+  this->i2cConfig.frequency = frequency;
 }
 
 SSD1306::LCD::~LCD() { turnOff(); }
 
 bool SSD1306::LCD::sendCommand(const unsigned char cmd) {
-    unsigned char msg[] = {COMMAND, cmd};
-    if (i2c->send(i2cConfig, (char*)msg, sizeof(msg))) {
-        return true;
-    }
+  unsigned char msg[] = {COMMAND, cmd};
+  if (i2c->send(i2cConfig, (char*)msg, sizeof(msg))) {
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 bool SSD1306::LCD::sendData(const unsigned char* data, int size) {
-    for (int i = 0; i < size; ++i) {
-        unsigned char msg[] = {DATA, data[i]};
-        if (!i2c->send(i2cConfig, (char*)msg, sizeof(msg))) {
-            return false;
-        }
+  for (int i = 0; i < size; ++i) {
+    unsigned char msg[] = {DATA, data[i]};
+    if (!i2c->send(i2cConfig, (char*)msg, sizeof(msg))) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 bool SSD1306::LCD::init() {
-    if (i2c->init()) {
-        int success = 0;
-        int count = 0;
-        isOn = true;
-        contrast = 0;
-        // Initialization sequence
-        success += turnOff();
-        count++;
-        success += setMultiplexRatio(height - 1);
-        count++;
-        success += sendCommand(SET_DISPLAY_OFFSET);
-        count++;
-        success += sendCommand(0x00);
-        count++;
-        success += sendCommand(SET_SEGMENT_REMAP_OFF);
-        count++;
+  if (i2c->init()) {
+    int success = 0;
+    int count = 0;
+    isOn = true;
+    contrast = 0;
+    // Initialization sequence
+    success += turnOff();
+    count++;
+    success += setMultiplexRatio(height - 1);
+    count++;
+    success += sendCommand(SET_DISPLAY_OFFSET);
+    count++;
+    success += sendCommand(0x00);
+    count++;
+    success += sendCommand(SET_START_LINE);
+    count++;
+    success += sendCommand(SET_SEGMENT_REMAP_OFF);
+    count++;
+    success += sendCommand(SET_COM_OUTPUT_SCAN_NORMAL);
+    count++;
+    success += sendCommand(SET_COM_PINS_HW_CONFIG);
+    count++;
+    success += sendCommand(COM_PINS_SEQUENTIAL);
+    count++;
+    success += setContrast(DEFAULT_CONTRAST);
+    count++;
+    success += sendCommand(ENTIRE_DISPLAY_ON_RESUME_RAM);
+    count++;
+    success += sendCommand(NORMAL_DISPLAY);
+    count++;
+    success += sendCommand(SET_DISPLAY_CLOCK_DIVIDER);
+    count++;
+    success += sendCommand(DEFAULT_DISPLAY_CLOCK_DIVIDER);
+    count++;
+    success += sendCommand(CHARGE_PUMP_SETTING);
+    count++;
+    success += sendCommand(CHARGE_PUMP_ENABLE);
+    count++;
+    success += setMemoryAddressingMode(MEMORY_ADDRESSING_VERTICAL);
+    count++;
+    success += sendCommand(DEACTIVATE_SCROLL);
+    count++;
+    success += clearScreen();
+    count++;
+    success += turnOn();
+    count++;
 
-        success += sendCommand(SET_DISPLAY_CLOCK_DIVIDER);
-        count++;
-        success += sendCommand(DEFAULT_DISPLAY_CLOCK_DIVIDER);
-        count++;
-
-        success += sendCommand(SET_START_LINE);
-        count++;
-        success += sendCommand(CHARGE_PUMP_SETTING);
-        count++;
-        success += sendCommand(CHARGE_PUMP_ENABLE);
-        count++;
-        success += setMemoryAddressingMode(MEMORY_ADDRESSING_VERTICAL);
-        count++;
-
-        success += sendCommand(SET_COM_OUTPUT_SCAN_NORMAL);
-        count++;
-        success += sendCommand(SET_COM_PINS_HW_CONFIG);
-        count++;
-        success += sendCommand(0x12);
-        count++;
-        success += setContrast(DEFAULT_CONTRAST);
-        count++;
-        success += sendCommand(SET_PRECHARGE_PERIOD);
-        count++;
-        success += sendCommand(0xF1);
-        count++;
-        success += sendCommand(SET_VCOM_DESELECT_LEVEL);
-        count++;
-        success += sendCommand(0x40);
-        count++;
-        success += sendCommand(DISPLAYALLON_RESUME);
-        count++;
-        success += sendCommand(NORMAL_DISPLAY);
-        count++;
-        success += sendCommand(DEACTIVATE_SCROLL);
-        count++;
-        success += turnOn();
-        count++;
-        success += setColumnRange(0x00, width - 1);
-        count++;
-        success += setPageRange(0x00, 0xFF);
-        count++;
-        success += clearScreen();
-        count++;
-
-        return success == count;
-    } else {
-        return false;
-    }
+    return success == count;
+  } else {
+    return false;
+  }
 }
 
 // Align writing area and draw x bitmap image
 bool SSD1306::LCD::drawXbitmap(const unsigned char* bitmap, int x, int y,
                                int width, int height) {
-    return setWritingArea(x, y, width, height) &&
-           sendData(bitmap, width * height / 8);
+  return setWritingArea(x, y, width, height) &&
+         sendData(bitmap, width * height / 8);
 }
 
 bool SSD1306::LCD::drawXbitmap(Graphic* graphic) {
-    return drawXbitmap(graphic->getData(), 0, 0, graphic->getWidth(),
-                       graphic->getHeight());
+  return drawXbitmap(graphic->getData(), 0, 0, graphic->getWidth(),
+                     graphic->getHeight());
 }
 
 bool SSD1306::LCD::drawXbitmap(Graphic graphic) {
-    return drawXbitmap(graphic.getData(), 0, 0, graphic.getWidth(),
-                       graphic.getHeight());
+  return drawXbitmap(graphic.getData(), 0, 0, graphic.getWidth(),
+                     graphic.getHeight());
 }
 
 // Writing area for text needs to be set up as textboxes with matching height of
 // the font
 bool SSD1306::LCD::writeText(const char* text, int length) {
-    int dataWritten = 0;
-    for (; *text != '\0'; ++text) {
-        int index = *text - ASCII_OFFSET;
+  int dataWritten = 0;
+  for (; *text != '\0'; ++text) {
+    int index = *text - ASCII_OFFSET;
 
-        if (index >= SANS_SERIF_16PX_POINTERS_LENGTH) {
-            continue;
-        }
-
-        unsigned char* iterator = SANS_SERIF_16PX_POINTERS[index];
-        for (; iterator != SANS_SERIF_16PX_POINTERS[index + 1]; ++iterator) {
-            unsigned char msg[] = {DATA, *iterator};
-            if (!i2c->send(i2cConfig, (char*)msg, sizeof(msg))) {
-                return false;
-            }
-
-            dataWritten++;
-        }
+    if (index >= SANS_SERIF_16PX_POINTERS_LENGTH) {
+      continue;
     }
 
-    unsigned char pad[length - dataWritten];
-    memset(pad, 0, sizeof(pad));
-    sendData(pad, sizeof(pad));
+    unsigned char* iterator = SANS_SERIF_16PX_POINTERS[index];
+    for (; iterator != SANS_SERIF_16PX_POINTERS[index + 1]; ++iterator) {
+      unsigned char msg[] = {DATA, *iterator};
+      if (!i2c->send(i2cConfig, (char*)msg, sizeof(msg))) {
+        return false;
+      }
 
-    return true;
+      dataWritten++;
+    }
+  }
+
+  unsigned char pad[length - dataWritten];
+  memset(pad, 0, sizeof(pad));
+  sendData(pad, sizeof(pad));
+
+  return true;
 }
 
 bool SSD1306::LCD::setWritingArea(int x, int y, int width, int height) {
-    int success = 0;
-    success += sendCommand(SET_COLUMN_ADDRESS);
-    success += sendCommand(x);
-    success += sendCommand(x + (width - 1));
-    success += sendCommand(SET_PAGE_ADDRESS);
-    int pageStart = (unsigned char)y / 8;
-    success += sendCommand(pageStart);
-    success += sendCommand(pageStart + (height / 8) - 1);
+  int success = 0;
+  int count = 0;
+  success += setColumnRange(x, x + (width - 1));
+  count++;
+  int pageStart = (unsigned char)y / 8;
+  success += setPageRange(pageStart, pageStart + (height / 8) - 1);
+  count++;
 
-    return success == 6;
+  return success == count;
 }
 
 bool SSD1306::LCD::setWritingArea(SSD1306::TextBox textBox) {
-    return setWritingArea(textBox.x(), textBox.y(), textBox.width(),
-                          textBox.height());
+  return setWritingArea(textBox.x(), textBox.y(), textBox.width(),
+                        textBox.height());
 }
 
 bool SSD1306::LCD::fillRectangle(int x, int y, int width, int height,
                                  bool color) {
-    int size = width * height / 8;
-    unsigned char data;
+  int size = width * height / 8;
+  unsigned char data;
 
-    if (color) {
-        data = 0xff;
-    } else {
-        data = 0x00;
+  if (color) {
+    data = 0xff;
+  } else {
+    data = 0x00;
+  }
+
+  if (!setWritingArea(x, y, width, height)) {
+    return false;
+  }
+
+  for (int i = 0; i < size; ++i) {
+    unsigned char msg[] = {DATA, data};
+    if (!i2c->send(i2cConfig, (char*)msg, sizeof(msg))) {
+      return false;
     }
+  }
 
-    if (!setWritingArea(x, y, width, height)) {
-        return false;
-    }
-
-    for (int i = 0; i < size; ++i) {
-        unsigned char msg[] = {DATA, data};
-        if (!i2c->send(i2cConfig, (char*)msg, sizeof(msg))) {
-            return false;
-        }
-    }
-
-    return true;
+  return true;
 }
 
 bool SSD1306::LCD::print(TextBox textBox, const char* text) {
-    return setWritingArea(textBox) && writeText(text, textBox.size());
+  return setWritingArea(textBox) && writeText(text, textBox.size());
 }
 
 bool SSD1306::LCD::clearScreen() {
-    unsigned char buffer[size];
-    memset(buffer, 0x00, size);
+  unsigned char buffer[size];
+  memset(buffer, 0x00, size);
+  int success = 0;
+  int count = 0;
+  success += setColumnRange(0x00, 0xFF);
+  count++;
+  success += setPageRange(0x00, 0xFF);
+  count++;
+  success += sendData(buffer, size);
+  count++;
 
-    return sendData(buffer, size);
+  return success == count;
 }
 
 bool SSD1306::LCD::turnOff() {
-    if (isOn && sendCommand(SSD1306::DISPLAY_OFF)) {
-        isOn = false;
-        return true;
-    }
+  if (isOn && sendCommand(SSD1306::DISPLAY_OFF)) {
+    isOn = false;
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 bool SSD1306::LCD::turnOn() {
-    if (!isOn && sendCommand(SSD1306::DISPLAY_ON)) {
-        isOn = true;
-        return true;
-    };
+  if (!isOn && sendCommand(SSD1306::DISPLAY_ON)) {
+    isOn = true;
+    return true;
+  };
 
-    return false;
+  return false;
 }
 
-bool SSD1306::LCD::lcdOn() { return isOn; }
+bool SSD1306::LCD::lcdIsOn() { return isOn; }
 
 bool SSD1306::LCD::setContrast(unsigned char value) {
-    if (contrast == value) {
-        return false;
-    }
-
-    int success = 0;
-    success += sendCommand(SSD1306::SET_CONTRAST);
-    success += sendCommand(value);
-
-    if (success == 2) {
-        contrast = value;
-        return true;
-    }
-
+  if (contrast == value) {
     return false;
+  }
+
+  int success = 0;
+  int count = 0;
+  success += sendCommand(SSD1306::SET_CONTRAST);
+  count++;
+  success += sendCommand(value);
+  count++;
+
+  if (success == count) {
+    contrast = value;
+    return true;
+  }
+
+  return false;
+}
+
+bool SSD1306::LCD::entireDisplayOn(bool resumeRam) {
+  if (resumeRam) {
+    return sendCommand(ENTIRE_DISPLAY_ON_RESUME_RAM);
+  }
+  return sendCommand(ENTIRE_DISPLAY_ON_IGNORE_RAM);
 }
 
 bool SSD1306::LCD::setMultiplexRatio(unsigned char ratio) {
-    int success = 0;
-    success += sendCommand(SET_MUX_RATIO);
-    success += sendCommand(ratio);
+  int success = 0;
+  int count = 0;
+  success += sendCommand(SET_MUX_RATIO);
+  count++;
+  success += sendCommand(ratio);
+  count++;
 
-    return success == 2;
+  return success == count;
 }
 
 bool SSD1306::LCD::setMemoryAddressingMode(unsigned char mode) {
-    int success = 0;
-    success += sendCommand(SET_MEMORY_ADDRESSING_MODE);
-    success += sendCommand(mode);
+  int success = 0;
+  int count = 0;
+  success += sendCommand(SET_MEMORY_ADDRESSING_MODE);
+  count++;
+  success += sendCommand(mode);
+  count++;
 
-    return success == 2;
+  return success == count;
 }
 
 bool SSD1306::LCD::invert() {
-    if (inverted) {
-        if (sendCommand(NORMAL_DISPLAY)) {
-            inverted = false;
-            return true;
-        } else {
-            return false;
-        }
+  if (inverted) {
+    if (sendCommand(NORMAL_DISPLAY)) {
+      inverted = false;
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    if (sendCommand(INVERT_DISPLAY)) {
-        inverted = true;
-        return true;
-    }
+  if (sendCommand(INVERT_DISPLAY)) {
+    inverted = true;
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 bool SSD1306::LCD::setColumnRange(unsigned char start, unsigned char end) {
-    int success = 0;
-    success += sendCommand(SET_COLUMN_ADDRESS);
-    success += sendCommand(start);
-    success += sendCommand(end);
+  int success = 0;
+  int count = 0;
+  success += sendCommand(SET_COLUMN_ADDRESS);
+  count++;
+  success += sendCommand(start);
+  count++;
+  success += sendCommand(end);
+  count++;
 
-    return success == 3;
+  return success == count;
 }
 
 bool SSD1306::LCD::setPageRange(unsigned char start, unsigned char end) {
-    int success = 0;
-    int count = 0;
-    success += sendCommand(SET_PAGE_ADDRESS);
-    count++;
-    success += sendCommand(start);
-    count++;
-    success += sendCommand(end);
-    count++;
+  int success = 0;
+  int count = 0;
+  success += sendCommand(SET_PAGE_ADDRESS);
+  count++;
+  success += sendCommand(start);
+  count++;
+  success += sendCommand(end);
+  count++;
 
-    return success == count;
+  return success == count;
+}
+
+bool SSD1306::LCD::setChargePumpSetting(unsigned char setting) {
+  int success = 0;
+  int count = 0;
+  success += sendCommand(CHARGE_PUMP_SETTING);
+  count++;
+  success += sendCommand(setting);
+  count++;
+
+  return success == count;
 }
 
 int SSD1306::LCD::lcdWidth() { return width; }
